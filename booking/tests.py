76424bd89927ce84cf9b3d835c1d6b571efd1f54
Anthony Christoforou <your.email@example.com>
2025-06-26 12:33:59 +0300

    Model test added and tested in terminal

--- booking/tests.py
+++ booking/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from booking.forms import BookingForm
+from booking.models import Booking
 from datetime import date
 
 # Create your tests here.
 
+# Form Tests
 class TestBookingForm(TestCase):
 
     def test_valid_form(self):
@@ -26,3 +28,23 @@ class TestBookingForm(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('full_name', form.errors)
         self.assertIn('room_type', form.errors)
+
+# Model Tests
+class BookingModelTest(TestCase):
+
+    def setUp(self):
+        self.booking = Booking.objects.create(
+            full_name="John Doe",
+            email="john@example.com",
+            room_type="double",
+            check_in=date(2025, 7, 1),
+            check_out=date(2025, 7, 5),
+            special_requests="High floor"
+        )
+
+    def test_booking_str_method(self):
+        self.assertEqual(str(self.booking), "John Doe (double)")
+
+    def test_booking_fields(self):
+        self.assertEqual(self.booking.room_type, "double")
+        self.assertEqual(self.booking.check_in, date(2025, 7, 1))
