968f9acbe125f6f6d2155b9886ec0a975525e0dd
Anthony Christoforou <your.email@example.com>
2025-06-27 00:43:39 +0300

    Enable django-allauth with registration

--- booking_project/urls.py
+++ booking_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # enables login/logout
+    path('accounts/', include('allauth.urls')),  # enables login/logout
 ]
