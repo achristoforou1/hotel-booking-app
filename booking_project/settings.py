58898950a78858e871e5de458054255263c4c347
Anthony Christoforou <your.email@example.com>
2025-06-26 19:17:35 +0300

    postgres database created and datbase replaced to prepare for heroku deplyment

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +78,7 @@ WSGI_APPLICATION = 'booking_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
---
+++ env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_NMXtCkS4vxT0@ep-curly-moon-a2ekhm6v.eu-central-1.aws.neon.tech/dash_opal_user_493324")
+
+os.environ.setdefault(
+    "SECRET_KEY", "!4g!u!p2!6exp)aur@mu+1=!qd70h!1$5-dktlh6&nycb430mv"
+)
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://137686235245989:gZT58RRxvqqSxOBez6tV6eJlmPI@drdmyurwv"
+)
