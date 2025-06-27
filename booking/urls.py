c7dbf1e99c28bc6cf76838ecf9a6a2d699155d47
Anthony Christoforou <your.email@example.com>
2025-06-28 00:41:09 +0300

    Override PasswordResetView to use custom template

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,5 +10,9 @@ urlpatterns = [
     path('my-bookings/', views.my_bookings, name='my_bookings'),
     path('edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
     path('delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path("accounts/password/reset/", auth_views.PasswordResetView.as_view(
+        template_name="registration/password_reset_form.html"
+    ), name="account_reset_password"),
+
     
 ]
