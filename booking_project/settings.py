a3f2d0f96256fa532de3648d64c8c886ac4455ba
Anthony Christoforou <your.email@example.com>
2025-06-28 11:26:51 +0300

    Fix: Include booking app templates directory in TEMPLATES DIRS

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -64,7 +64,10 @@ ROOT_URLCONF = 'booking_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'booking', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
