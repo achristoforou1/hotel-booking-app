a0c01a45311512907275c31ab890fc1125eadc2c
Anthony Christoforou <your.email@example.com>
2025-06-28 11:07:38 +0300

    Cleanup: Revert to default allauth login/signup paths and remove conflicting templates

--- booking_project/urls.py
+++ booking_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('accounts/', include('booking.accounts_urls')),  # custom routing
+    path('accounts/', include('allauth.urls')),
     path('', include('booking.urls')),
 ]
 
