b6b24516b1d9bfbc33bce37627c2c270772bb52c
Anthony Christoforou <your.email@example.com>
2025-06-30 16:01:56 +0300

    Cleanup: Removed unused MESSAGE_TAGS from settings.py

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -138,12 +138,11 @@ ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 # Whitenoise static files settings
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
