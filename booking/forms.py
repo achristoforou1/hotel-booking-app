269f7bf33865d0af9890cb13cd9693be411428b6
Anthony Christoforou <your.email@example.com>
2025-06-30 11:01:03 +0300

    validation error added to edit booking so check out date must be after check-in

--- booking/forms.py
+++ booking/forms.py
@@ -1,6 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Booking
-
 
 class BookingForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,11 @@ class BookingForm(forms.ModelForm):
             'check_in': forms.DateInput(attrs={'type': 'date'}),
             'check_out': forms.DateInput(attrs={'type': 'date'}),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        check_in = cleaned_data.get('check_in')
+        check_out = cleaned_data.get('check_out')
+
+        if check_in and check_out and check_out <= check_in:
+            raise ValidationError("Check-out date must be after check-in date.")
